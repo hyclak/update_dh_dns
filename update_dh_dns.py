1c4194f78b7c7ff009256ac36e9c356ec6e33268
Matt Hyclak <hyclak@gmail.com>
2022-08-30 10:55:40 -0400

    Fix linting

--- update_dh_dns.py
+++ update_dh_dns.py
@@ -1,45 +1,55 @@
 #!/usr/bin/env python3
+""" Update DreamHost DNS Entry from Environment Variables """
 
 import os
 import re
 import sys
 from urllib.request import urlopen
 
+APIURL = "https://api.dreamhost.com"
+
 def get_current_ip():
-    ip = urlopen('https://api.ipify.org').read().decode('utf8')
-    return str(ip)
+    """ Get external IP address from api.ipify.org """
+    with urlopen('https://api.ipify.org').read().decode('utf8') as current_ip:
+        return str(current_ip)
 
 def update_dreamhost_a_record(key, record, new_ip):
-    records = urlopen(f"https://api.dreamhost.com/?key={key}&cmd=dns-list_records").read().decode('utf8')
-    # print(records)
-    record_match = rf"{re.escape(record)}\s+A\s+(.\d+\.\d+\.\d+\.\d+)\s+"
-    if not re.search(record_match, records):
-        print("No current record found. Adding Record.")
-    else: 
-        current_record_ip = re.split(record_match, records)[1]
-        if current_record_ip == new_ip:
-            print("IP record is already correct, no action taken.")
-            return
-        else:
-            result = urlopen(f"https://api.dreamhost.com/?key={key}&cmd=dns-remove_record&record={record}&type=A&value={current_record_ip}").read().decode('utf8')
-            print(f"Removing old IP ({current_record_ip}): {result}")
+    """ Update A record via DreamHost DNS API """
+    records_url = f"{APIURL}/?key={key}&cmd=dns-list_records"
+    with urlopen(records_url).read().decode('utf8') as records:
 
-    # Finally, add Record
-    result = urlopen(f"https://api.dreamhost.com/?key={key}&cmd=dns-add_record&record={record}&type=A&value={new_ip}").read().decode('utf8')
-    print(f"Adding new IP ({new_ip}): {result}")
+        record_match = rf"{re.escape(record)}\s+A\s+(.\d+\.\d+\.\d+\.\d+)\s+"
+        if not re.search(record_match, records):
+            print("No current record found. Adding Record.")
+        else:
+            current_record_ip = re.split(record_match, records)[1]
+            if current_record_ip == new_ip:
+                print("IP record is already correct, no action taken.")
+                return
+
+            # Remove existing record
+            remove_url = f"{APIURL}/?key={key}&cmd=dns-remove_record&record={record}&type=A&value={current_record_ip}" # pylint: disable=C0301
+            with urlopen(remove_url).read().decode('utf8') as result:
+                print(f"Removing old IP ({current_record_ip}): {result}")
+
+        # Finally, add Record
+        add_url = f"{APIURL}/?key={key}&cmd=dns-add_record&record={record}&type=A&value={new_ip}"
+        with urlopen(add_url).read().decode('utf8') as result:
+            print(f"Adding new IP ({new_ip}): {result}")
 
 def main():
+    """ Initiate Update """
     try:
         apikey = os.environ['DH_API_KEY']
-    except:
+    except KeyError():
         print("Please define the DH_API_KEY environment variable with a valid Dreamhost API Key.")
-        exit(1)
+        sys.exit(1)
 
     try:
         record = os.environ['DH_HOSTNAME']
-    except:
+    except KeyError():
         print("Please define the DH_HOSTNAME environment variable with a valid hostname.")
-        exit(1)
+        sys.exit(1)
 
     external_ip = get_current_ip()
     print(f"Updating IP to {external_ip}")
